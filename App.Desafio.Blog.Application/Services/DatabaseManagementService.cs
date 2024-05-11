376a04092458802bee9d5d7a7a0438b4a91bf13a
Marcio Takahashi <marcio.takahashi.br@outlook.com>
2024-05-11 13:45:05 -0300

    feat: Ajustes

---
+++ App.Desafio.Blog.Application/Services/DatabaseManagementService.cs
@@ -0,0 +1,24 @@
+﻿using App.Desafio.Blog.Infra.Data.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Desafio.Blog.Application.Services
+{
+    public static class DatabaseManagementService
+    {
+        public static void MigrationInit(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<AppDbContext>();
+                if (context.Database.GetPendingMigrations().Any())
+                {
+                    context.Database.Migrate();
+                }
+            }
+        }
+    }
+}
